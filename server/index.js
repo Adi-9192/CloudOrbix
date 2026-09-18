d1cf7e49ce60e458683fd4b44ad2765853adc04f
Adi-9192 <aditya.swaroop-singh@capgemini.com>
2026-09-18 15:57:46 +0530

    Import containerClient from storage.js

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ import reportRoutes from './routes/reports.js';
 import projectRoutes from './routes/projects.js';
 import serviceRoutes from './routes/services.js';
 import profileTasksRoutes from './routes/profile-tasks.js';
+import { containerClient } from "./storage.js";
 import { initializeDatabase } from './db.js';
 
 const app = express();
